d67eae3ff5b15d1148dcb2f448df2b204fd2a3d7
Muhammad Waqar <58720926+vickyrajput5@users.noreply.github.com>
2023-01-27 20:14:04 +0500

    27/1/2023

--- src/Component/Reserve/Reserve.jsx
+++ src/Component/Reserve/Reserve.jsx
@@ -2,10 +2,27 @@ import "./Reserve.css"
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import { faCircleXmark } from "@fortawesome/free-solid-svg-icons"
 import useFatch from "../../Hooks/useFatch"
-import { useState } from "react"
+import { useContext, useState } from "react"
+import { SearchContext } from "../../Context/Searchcontext"
 const Reserve = ({setOpen, hotelId}) => {
     const [selectRoom, setSelectRoom] = useState([])
     const  {data, loading, error,reFatch } = useFatch(`/hotel/room/${hotelId}`)
+    const {date} = useContext(SearchContext)
+
+    const getDateRange = (startDate, endDate) =>{
+        const start = new Date(startDate);
+        const end =  new Date(endDate)
+         const dates = new Date(start.getTime());
+
+         let date = [];
+         while(date <= end){
+             date.push(new Date(dates).getTime())
+             dates.setDate(dates.getDate() + 1)
+         }
+         return date
+    }
+    const allDates = getDateRange(date[0].startDate, date[0].endDate)
+
     
     const handleSelect = (e) =>{
         const checked = e.target.checked;
@@ -13,6 +30,9 @@ const Reserve = ({setOpen, hotelId}) => {
         setSelectRoom(
             checked ? [...selectRoom, value] : selectRoom.filter((item)=> item !== value)
         )
+    }
+    const handleClick =()=>{
+
     }
 console.log(selectRoom);
   return (
@@ -38,6 +58,7 @@ console.log(selectRoom);
         ))
 
         }
+        <button onClick={handleClick} className="rButton">Reserve Now!</button>
         </div>
         </div>
   )
